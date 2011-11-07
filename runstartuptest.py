59bffc14e2d106f68f43d30488e2d0a0306ffc27
Clint Talbert <ctalbert@mozilla.com>
2011-11-07 15:30:14 -0800

    Added config file and sut agent to make more configurable and stable

--- runstartuptest.py
+++ runstartuptest.py
@@ -3,6 +3,10 @@ import subprocess
 import os
 import optparse
 import sys
+import ConfigParser
+
+import devicemanager,devicemanagerSUT
+from devicemanager import NetworkTools
 
 # These are configurable parameters
 
@@ -38,9 +42,21 @@ class StartupOptions(optparse.OptionParser):
         optparse.OptionParser.__init__(self, **kwargs)
         defaults = {}
         
+        self.add_option("--configFile", action="store", type="string", dest="configfile",
+                        help="Path to configuration file, if specified then no other options take effect")
+        defaults["configfile"] = None
+        
         self.add_option("--serverIP", action="store", type="string", dest="serverip",
                         help="IP and port of server to report results to i.e. 192.168.1.4:8080")
         defaults["serverip"] = None
+        
+        self.add_option("--deviceIP", action="store", type="string", dest="deviceip",
+                        help="IP address of phone, assumes phone running SUTAgent")
+        defaults["deviceip"] = None
+        
+        self.add_option("--devicePort", action="store", type="string", dest="deviceport",
+                        help="Port of agent on phone, assumes 20701")
+        defaults["deviceport"] = 20701
         
         self.add_option("--phone", action="store", type="string", dest="phoneid",
                         help="Text to identify the phone in test for results reporting")
@@ -71,6 +87,10 @@ class StartupOptions(optparse.OptionParser):
                               They will be copied to testroot/. Defaults to local directory")
         defaults["htmldir"] = os.getcwd()
         
+        self.add_option("--runtype", action="store", type="string", dest="runtype",
+                        help="Whether to do 'cold' or 'warm' startup test, defaults to cold")
+        defaults["runtype"] = ["cold"]
+        
         self.set_defaults(**defaults)
         
         usage = """\
@@ -85,7 +105,60 @@ wish to test should be already installed, and the reporting server should alread
 be running."""
         self.set_usage(usage)
 
+    def read_config(self, configfile):
+        # Reads every option from config file, fails if exepected options are
+        # not found in the config file
+        config = ConfigParser.RawConfigParser()
+        config.read(configfile)
+        options = {}
+        try:
+            u = {}
+            for i in config.items("urls"):
+                u[i[0]] = i[1]
+            options["urls"] = u
+        except:
+            print "Problem reading in urls from config file: %s %s" % sys.exc_info()[:2]
+        
+        try:
+            a = {}
+            for i in config.items("apps"):
+                a[i[0]] = i[1]
+            options["apps"] = a
+        except:
+            print "Problem reading in apps from config file: %s %s" % sys.exc_info()[:2]
+        
+        try:
+            h = []
+            for i in config.items("htmlfiles"):
+                h.append(i[1])
+            options["htmlfiles"] = h
+        except:
+            print "Problem reading in htmlfiles from config file: %s %s" % sys.exc_info()[:2]
+        
+        try:
+            for i in config.items("options"):
+                options[i[0]] = i[1]
+        except:
+            print "Problem reading in main options section from config file: %s %s" % sys.exc_info()[:2]
+        
+        if not options["htmldir"]:
+            options["htmldir"] = os.getcwd()
+        
+        options["runtype"] = options["runtype"].split(",")
+        
+        if not options["daemon-mode"]:
+            options["daemon-mode"] = False
+        else:
+            options["daemon-mode"] = True
+
+        print "DBG: options are: %s" % options
+        return options
+        
     def verify_options(self, options):
+        if options.configfile and os.path.exists(options.configfile):
+            options = self.read_config(options.configfile)
+            return options
+
         if not options.serverip:
             print "You must specify a server IP address for result reporting"
             sys.exit(1)
@@ -98,32 +171,47 @@ be running."""
             print "You must specify the path to the android sdk"
             sys.exit(1)
         
-        #TODO: If we decide to load in the urls and browsers from a file, then
-        #      this will be a great place to do that and we can hang those structs
-        #      off the options object and get rid of the globals above.
-        #      So, in preparation for modifying the options object in this function
-        #      we return it.
+        print "opts: %s" % options
         return options
 
 
 class StartupTest:
-    def __init__(self, options):
-        self.adb = os.path.join(options.sdk, "platform-tools", "adb")
-        self.script = options.script
-        self.timecmd = options.timecmd
-        self.testroot = options.testroot
-        self.serverip = options.serverip
-        self.phoneid = options.phoneid
-        self.htmldir = options.htmldir
+    def __init__(self, dm, options):
+        self.dm = dm
+        self.adb = os.path.join(options["sdk"], "platform-tools", "adb")
+        self.script = options["script"]
+        self.timecmd = options["timecmd"]
+        self.testroot = options["testroot"]
+        self.serverip = options["serverip"]
+        self.phoneid = options["phoneid"]
+        self.htmldir = options["htmldir"]
+        self.runtype = options["runtype"]
+        
+        if options["urls"]:
+            self.urls = options["urls"]
+        else:
+            self.urls = urls
+        
+        if options["apps"]:
+            self.apps = options["apps"]
+        else:
+            self.apps = apps
+        
+        if options["htmlfiles"]:
+            self.htmlfiles = options["htmlfiles"]
+        else:
+            self.htmlfiles = htmlfiles
+        
         
     def prepare_phone(self):
         try:
             # Create our testroot
             cmd = ["mkdir %s" % self.testroot]
             msg = self._run_adb("shell", cmd)
-            if msg and not "File exists" in msg:
-                raise Exception('MakeTestRoot', 'Cannot make testroot directory')
+            #if msg and not "File exists" in msg:
+            #    raise Exception('MakeTestRoot', 'Cannot make testroot directory')
             
+            print "done with mkdir: %s" % msg
             # Copy our time script into place
             m = self._run_adb("push", [self.timecmd, "/data/local"])
             print "copy time: %s" % m
@@ -137,9 +225,7 @@ class StartupTest:
             print "copy script: %s" % m
             
             # Copy our HTML files for local use into place
-            # TODO: This is hardcoded right now to files in the htmlfiles array (see above)
-            #       We may want to make this a configuration file too
-            for f in htmlfiles:
+            for f in self.htmlfiles:
                 m = self._run_adb("push", [os.path.join(self.htmldir, f), self.testroot])
                 print "copy html files: %s" % m
             
@@ -150,12 +236,12 @@ class StartupTest:
             print "Failed to prepare phone due to %s" % e
             sys.exit(1)
         
-    def run(self):
+    def run(self, runtype="cold"):
         # Assume the script has been pushed to the phone, set up the path for adb
         phonescript = self.testroot + "/" + os.path.split(self.script)[1]
         
-        for browser, app in apps.iteritems():
-            for testname, url in urls.iteritems():
+        for browser, app in self.apps.iteritems():
+            for testname, url in self.urls.iteritems():
                 if 'local' in testname:
                     # Then add in testroot as the server location in URL
                     u = url % (self.testroot, self.serverip, self.phoneid, testname, browser)
@@ -163,21 +249,39 @@ class StartupTest:
                     # Then add in the server twice (once for URL, once for param)
                     u = url % (self.serverip, self.serverip, self.phoneid, testname, browser)
                  
-                out = self._run_adb("shell", ["sh", phonescript, app, u])
-                print out
-                # Give the html 10s to upload results
-                sleep(10)
+                #out = self._run_adb("shell", ["sh %s %s %s" % (phonescript, app, u)], inshell=True)
+                #out = self._run_adb("shell", ["sh", phonescript, app, u])
+                cmd = ["sh", phonescript, app, u]
+                self.dm.launchProcess(cmd)
 
-                # The name of the process is to the left of the / in the activity manager string
-                out = self._run_adb("shell", ["kill", app.split("/")[0]])
-                print out
+                # Give the html 5s to upload results
+                sleep(5)
+                print "Time up"
+
+                if runtype == "cold":
+                    # reboot the device between runs
+                    print "Rebooting device"
+                    nettools = NetworkTools()
+                    self.dm.reboot(nettools.getLanIp())
+                    print "ok, done with reboot"
+                else:
+                    print "Killing app"
+                    # Then we do a warm startup, killing the process between runs
+                    # The name of the process is to the left of the / in the activity manager string
+                    self.dm.killProcess(app.split("/")[0])
     
     # cmd must be an array!
-    def _run_adb(self, adbcmd, cmd):
+    def _run_adb(self, adbcmd, cmd, inshell=False):
         print "run adb cmd: %s" % subprocess.list2cmdline([self.adb, adbcmd] + cmd)
-        p = subprocess.Popen([self.adb, adbcmd] + cmd,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
+        if (inshell):
+            p = subprocess.Popen([self.adb, adbcmd] + cmd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT,
+                                 shell=True)
+        else:
+            p = subprocess.Popen([self.adb, adbcmd] + cmd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
         return p.communicate()[0]
 
 def main():
@@ -185,10 +289,19 @@ def main():
     options, args = parser.parse_args()
     options = parser.verify_options(options)
     
-    # Run it
-    startuptest = StartupTest(options)
-    startuptest.prepare_phone()
-    startuptest.run()
+    # Get our devicemanager instantiated - use the agent to synchronously reboot
+    # and to exec processes on the device.
+    dm = devicemanagerSUT.DeviceManagerSUT(options["deviceip"], options["deviceport"])
+    
+    if options["daemon-mode"]:
+        # Then we go into daemon mode
+        print "Daemon mode, wait for phones to come to us"
+    else:
+        # Run it
+        startuptest = StartupTest(dm, options)
+        startuptest.prepare_phone()
+        for i in options["runtype"]:
+            startuptest.run(i)
 
 if __name__ == '__main__':
     main()
